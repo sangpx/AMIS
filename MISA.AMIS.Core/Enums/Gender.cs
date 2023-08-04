e690dd3148cf1a10357503d326bcebe15ca33ba7
sann <psang4845@gmail.com>
2023-08-04 16:17:06 +0700

    first upload

---
+++ MISA.AMIS.Core/Enums/Gender.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MISA.AMIS.Core.Enums
+{
+    /// <summary>
+    /// Giới Tính
+    /// CreateBy: SANG - 13/12/2022
+    /// </summary>
+    public enum Gender
+    {
+        /// <summary>
+        /// Nữ
+        /// </summary>
+        Female = 0,
+
+        /// <summary>
+        /// Nam
+        /// </summary>
+        Male = 1,
+
+        /// <summary>
+        /// Khác
+        /// </summary>
+        Other = 2
+    }
+}
